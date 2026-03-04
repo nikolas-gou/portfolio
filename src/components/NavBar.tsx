c12aae5f3be14ef01ebce1613142500768b04373
nikolas-gou <nikolasgou23@gmail.com>
2026-03-04 21:06:21 +0200

    Creating the Banner.tsx

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -77,10 +77,10 @@ export const NavBar = () => {
                         </Nav>
                         <span className="navbar-text">
                             <div className="social-icon">
-                                <a href="#">
+                                <a href="https://www.linkedin.com/in/nikolaosgkouziotis/">
                                     <img src={navIcon1} alt="" />
                                 </a>
-                                <a href="#">
+                                <a href="https://github.com/nikolas-gou">
                                     <img src={navIcon2} alt="" />
                                 </a>
                                 <a href="#">

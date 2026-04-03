16d8ff7e5ccbe3ad525cac8d9b9a2076690bb420
nikolas-gou <nikolasgou23@gmail.com>
2026-04-04 01:02:48 +0300

    Modifying Styling, layout of Projects page

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,4 @@
-import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
-import { ProjectCard } from "./ProjectCard";
+import { Container, Row, Col } from "react-bootstrap";
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
@@ -11,10 +10,7 @@ export const Projects = () => {
     const projects = [
         {
             title: "ThesisGate.net",
-            description: `A full-stack web application developed to manage theses, students, and instructors. The
-                platform streamlines thesis workflows, digitizes administrative processes, and facilitates
-                communication. Built with React, PHP-Slim, and structured database architecture for
-                department-wide deployment.`,
+            description: `Full-stack platform for managing thesis workflows, authentication, and role-based access.`,
             imgUrl: projImg1,
             skills: [
                 { name: "JS", img: "" },
@@ -54,6 +50,19 @@ export const Projects = () => {
                 { name: "Bootstrap", img: "" },
             ],
         },
+        {
+            title: "Mobile Movie App",
+            description:
+                "A React Native mobile app showcasing core concepts like navigation, API integration, and scalable architecture through a movie browsing experience.",
+            imgUrl: projImg3,
+            skills: [
+                { name: "TS", img: "" },
+                { name: "React-Native", img: "" },
+                { name: "React-Navigation", img: "" },
+                { name: "Cross-Platform", img: "" },
+                { name: "Expo-Router", img: "" },
+            ],
+        },
     ];
 
     return (
@@ -78,83 +87,34 @@ export const Projects = () => {
                                         problem-solving, usability, and
                                         performance.
                                     </p>
-                                    <Tab.Container
-                                        id="projects-tabs"
-                                        defaultActiveKey="first"
-                                    >
-                                        <Nav
-                                            variant="pills"
-                                            className="nav-pills mb-5 justify-content-center align-items-center"
-                                            id="pills-tab"
-                                        >
-                                            <Nav.Item>
-                                                <Nav.Link eventKey="first">
-                                                    Tab 1
-                                                </Nav.Link>
-                                            </Nav.Item>
-                                            <Nav.Item>
-                                                <Nav.Link eventKey="second">
-                                                    Tab 2
-                                                </Nav.Link>
-                                            </Nav.Item>
-                                            <Nav.Item>
-                                                <Nav.Link eventKey="third">
-                                                    Tab 3
-                                                </Nav.Link>
-                                            </Nav.Item>
-                                        </Nav>
-                                        <Tab.Content
-                                            id="slideInUp"
-                                            className={
-                                                isVisible
-                                                    ? "animate__animated animate__slideInUp"
-                                                    : ""
-                                            }
-                                        >
-                                            <Tab.Pane eventKey="first">
-                                                <Row>
-                                                    {projects.map(
-                                                        (project, index) => {
-                                                            return (
-                                                                <ProjectCard
-                                                                    key={index}
-                                                                    {...project}
-                                                                />
-                                                            );
-                                                        },
-                                                    )}
-                                                </Row>
-                                            </Tab.Pane>
-                                            <Tab.Pane eventKey="second">
-                                                {/* future  */}
-                                                <p>
-                                                    Lorem ipsum dolor sit amet
-                                                    consectetur adipisicing
-                                                    elit. Cumque quam, quod
-                                                    neque provident velit, rem
-                                                    explicabo excepturi id illo
-                                                    molestiae blanditiis,
-                                                    eligendi dicta officiis
-                                                    asperiores delectus quasi
-                                                    inventore debitis quo.
-                                                </p>
-                                            </Tab.Pane>
-                                            <Tab.Pane eventKey="third">
-                                                {/* future */}
-                                                <p>
-                                                    Lorem ipsum dolor sit amet
-                                                    consectetur adipisicing
-                                                    elit. Cumque quam, quod
-                                                    neque provident velit, rem
-                                                    explicabo excepturi id illo
-                                                    molestiae blanditiis,
-                                                    eligendi dicta officiis
-                                                    asperiores delectus quasi
-                                                    inventore debitis quo.
-                                                </p>
-                                            </Tab.Pane>
-                                        </Tab.Content>
-                                    </Tab.Container>
+
+                                    <Row>
+                                        <div className="project-boxes">
+                                            {projects.map((project, i) => (
+                                                <div
+                                                    className="project-card"
+                                                    key={i}
+                                                >
+                                                    <h4>{project.title}</h4>
+
+                                                    <p>{project.description}</p>
+
+                                                    <div className="project-skills">
+                                                        {project.skills.map(
+                                                            (skill, idx) => (
+                                                                <span
+                                                                    key={idx}
+                                                                    className="skill-tag"
+                                                                >
+                                                                    {skill.name}
+                                                                </span>
+                                                            ),
+                                                        )}
+                                                    </div>
+                                                </div>
+                                            ))}
+                                        </div>
+                                    </Row>
                                 </div>
                             )}
                         </TrackVisibility>
